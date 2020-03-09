c6d669f3a84c92553798a5bb8419a3d01677e92f
Brendan Cheshire <brendancheshire@gmail.com>
2020-03-09 08:28:30 +0000

    Added image push step

--- .scripts/push_docker_image.sh
+++ .scripts/push_docker_image.sh
@@ -22,4 +22,31 @@ if [[ "$should_exit" -eq 1 ]]; then
     exit 1
 fi
 
+base_image=
+
+# Parse command line args
+while (( "$#" )); do
+  case "$1" in
+    -b|--base-image)
+      base_image=$2
+      shift 2
+      ;;
+    -t|--target-image)
+      target_image=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Login and push image
 docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASSWORD
+docker tag "$base_image" "$target_image"
+docker push "$target_image"
